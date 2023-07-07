4bb0158c399c00af0d8411149fe77321bee1d8fe
albluc24 <albluc24@hotmail.com>
2023-07-07 12:55:04 +0000

    switch to sounfile to get audio file duration for performance, adding it to requirements

--- preprocess.py
+++ preprocess.py
@@ -1,16 +1,17 @@
-from tqdm import tqdm
+import soundfile as sf
 import os
 import sys
 import csv
-from pydub.utils import mediainfo
-from phonemizer.backend import EspeakBackend
 import hparams
+from phonemizer.backend import EspeakBackend
+from tqdm import tqdm
+
 def write_audio_files(input_folder):
     with open('train.txt', 'w') as output_file:
         for file_name in tqdm(os.listdir(input_folder)):
             if file_name.endswith('.wav'):
                 file_path = os.path.join(input_folder, file_name)
-                audio_info = mediainfo(file_path)
+                audio_info = sf.info(file_path).duration
                 output_file.write(f"{file_path}|{audio_info['duration']}\n")
     #creating eval.txt, that contains the last 5 lines from train.txt, and deleting them out of train.txt using bash
     os.system("""tail -n 5 train.txt > eval.txt && sed -i -e :a -e '$d;N;2,5ba' -e 'P;D' train.txt""")
